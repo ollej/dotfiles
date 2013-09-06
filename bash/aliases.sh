8e698996576d397207f34d69fd03c44d2f62e178
Olle Johansson <olle@avidity.se>
2013-09-06 12:38:59 +0200

    Add alias to update ctags

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -7,6 +7,7 @@ alias cvsup="cvs -q up -dP"
 alias sl=ls
 alias rack='ack --ruby --pager="less -R"'
 alias testing="rake db:drop db:create db:migrate db:test:prepare && rake i18n:import_translations db:seed; ls dependencies/promote-*/spec/dummy/db/schema.rb | xargs -n 1 cp -v db/schema.rb; cd dependencies/promote-i18n/ && bundle && rake app:spec; cd ../promote-models/ && bundle && rake app:spec; cd ../promote-builder && bundle && rake app:spec app:jasmine:headless; cd ../.. && bundle && rake konacha:run spec"
+alias updatectags="ctags -R --exclude=.git --exclude=log *"
 
 # Pretty ls
 export CLICOLOR=1
